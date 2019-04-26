9cb7e7dacd910c2f41ed19852cb8452fc0b377f7
EGOIST <0x142857@gmail.com>
2019-04-27 00:12:44 +0800

    test: more tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,19 +1,33 @@
 import { toRoutes } from '../src'
 
 test('simple', () => {
-  expect(toRoutes([
-    'index.vue',
-    'about.vue',
-    '[user].vue',
-    '[user]/index.vue',
-    '[user]/profile.vue',
-    'explore.vue',
-    'explore/tweets.vue',
-    'post/[post].vue',
-    'post/[post]/index.vue',
-    'post/[post]/[comment].vue',
-    'post/[post]/[comment]/index.vue',
-    'post/[post]/[comment]/likes.vue',
-    'post/[post]/[comment]/replies.vue',
-  ])).toMatchSnapshot()
+  expect(toRoutes(['index.vue', 'about.vue'])).toMatchSnapshot()
+})
+
+test('directory', () => {
+  expect(
+    toRoutes(['index.vue', 'foo/bar.vue', 'foo/baz.vue'])
+  ).toMatchSnapshot()
+})
+
+test('nesting routes', () => {
+  expect(toRoutes(['index.vue', 'foo.vue', 'foo/bar.vue', 'foo/baz.vue']))
+})
+
+test('complex example', () => {
+  expect(
+    toRoutes([
+      '[user].vue',
+      '[user]/index.vue',
+      '[user]/profile.vue',
+      'explore.vue',
+      'explore/tweets.vue',
+      'post/[post].vue',
+      'post/[post]/index.vue',
+      'post/[post]/[comment].vue',
+      'post/[post]/[comment]/index.vue',
+      'post/[post]/[comment]/likes.vue',
+      'post/[post]/[comment]/replies.vue'
+    ])
+  ).toMatchSnapshot()
 })
